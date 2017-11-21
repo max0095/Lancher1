57456b332959155f4dc085dd88f1aa3342944cf5
Kitpipat Somprasong <maxnutnaro@gmail.com>
2017-11-21 19:24:11 +0700

    Add project files.

---
+++ Lancher/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lancher.Startup))]
+namespace Lancher
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
